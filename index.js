638ef0ac06029ea11feffd97055519ffd0657cfa
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-07-26 09:12:42 +0530

    Updated iindex.js and index.html

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Thanks for sending data");
+});
 
 app.listen(3000, () => {
   console.log("server sstarted on port 3000");
